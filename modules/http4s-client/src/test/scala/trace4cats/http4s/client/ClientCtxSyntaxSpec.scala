835b8cd43cf93376687018fdf0fa2ab1ac95c598
sanjaya.subedi <sanjaya.subedi@mobimeo.com>
2023-06-14 16:23:59 +0200

    Reset test

--- modules/http4s-client/src/test/scala/trace4cats/http4s/client/ClientCtxSyntaxSpec.scala
+++ modules/http4s-client/src/test/scala/trace4cats/http4s/client/ClientCtxSyntaxSpec.scala
@@ -2,23 +2,14 @@ package trace4cats.http4s.client
 
 import cats.data.Kleisli
 import cats.effect.IO
-import org.typelevel.ci.CIStringSyntax
 import trace4cats.ToHeaders
 import trace4cats.http4s.client.syntax._
 import trace4cats.http4s.client.Instances._
 import trace4cats.http4s.common._
-import trace4cats.optics.Getter
 
 class ClientCtxSyntaxSpec
     extends BaseClientTracerSpec[IO, Kleisli[IO, TraceContext[IO], *], TraceContext[IO]](
       RunIOToId,
       TraceContext("3d86cad5-d321-448f-a758-d28714fc1045", _),
-      _.liftTraceContext(
-        spanLens = TraceContext.span[IO],
-        headersGetter = TraceContext.headers[IO](ToHeaders.all),
-        Http4sSpanNamer.methodWithPath,
-        Set(ci"foo.bar").contains,
-        Getter(_ => Map(("test.attribute", "foo"))
-        )
-      )
-    ){}
+      _.liftTraceContext(spanLens = TraceContext.span[IO], headersGetter = TraceContext.headers[IO](ToHeaders.all))
+    )
--- modules/http4s-client/src/test/scala/trace4cats/http4s/client/ClientSyntaxSpec.scala
+++ modules/http4s-client/src/test/scala/trace4cats/http4s/client/ClientSyntaxSpec.scala
@@ -7,4 +7,4 @@ import trace4cats.http4s.client.syntax._
 import trace4cats.http4s.common.RunIOToId
 
 class ClientSyntaxSpec
-    extends BaseClientTracerSpec[IO, Kleisli[IO, Span[IO], *], Span[IO]](RunIOToId, identity, _.liftTraceOld())
+    extends BaseClientTracerSpec[IO, Kleisli[IO, Span[IO], *], Span[IO]](RunIOToId, identity, _.liftTrace())
